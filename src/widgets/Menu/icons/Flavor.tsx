d06847cfbf9463a93499be0b71283a2ea5abe031
leadTeaswap <lead.teaswap@outlook.com>
2021-11-14 12:57:40 +0100

    update logo icon

---
+++ src/widgets/Menu/icons/Flavor.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Svg from "../../../components/Svg/Svg";
+import { SvgProps } from "../../../components/Svg/types";
+
+const Icon: React.FC<SvgProps> = (props) => {
+  return (
+    <Svg viewBox="0 0 24 24" {...props}>
+        <image
+          href="/images/egg/flavor.png"
+        />
+    </Svg>
+  );
+};
+
+export default Icon;
